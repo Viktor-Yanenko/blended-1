f19b7c73bf0073300a7a0d080976790150aa2854
Viktor <viktoryanenko11@gmail.com>
2025-03-13 13:12:36 +0200

    fix task 13

--- js/lesson-3.js
+++ js/lesson-3.js
@@ -232,50 +232,45 @@
 // Крім того, клас `Employee` має мати метод `getEmployeeDetails()`,
 // який повертає об'єкт з зарплатою співробітника та відділом, в якому він працює.
 
-// class Person {
-//   constructor(params) {
-//     this.name = params.name;
-//     this.age = params.age;
-//     this.gender = params.gender;
-//     this.email = params.email;
-//   }
-//   getDetails() {
-//     return {
-//       name: this.name,
-//       age: this.age,
-//       gender: this.gender,
-//       email: this.email,
-//     };
-//   }
-// }
+class Person {
+  constructor(name, age, gender, email) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.email = email;
+  }
+  getDetails() {
+    return {
+      name: this.name,
+      age: this.age,
+      gender: this.gender,
+      email: this.email,
+    };
+  }
+}
 
-// class Employee extends Person {
-//   constructor(args) {
-//     super({
-//       name: args.name,
-//       age: args.age,
-//       gender: args.gender,
-//       email: args.email,
-//     });
-//     this.salary = args.salary;
-//     this.department = args.department;
-//   }
+class Employee extends Person {
+  constructor(name, age, gender, email, salary, department) {
+    super(name, age, gender, email);
+    this.salary = salary;
+    this.department = department;
+  }
 
-//   getEmployeeDetails() {
-//     return {
-//       ...this.getDetails(),
-//       salary: this.salary,
-//       department: this.department,
-//     };
-//   }
-// }
+  getEmployeeDetails() {
+    return {
+      ...this.getDetails(),
+      salary: this.salary,
+      department: this.department,
+    };
+  }
+}
 
-// const viktor = new Employee({
-//   name: 'Viktor',
-//   age: 24,
-//   gender: 'male',
-//   email: 'viktoryanenko11@gmail.com',
-//   salary: 430,
-//   department: 'sales manager',
-// });
-// console.log(viktor.getEmployeeDetails());
+const viktor = new Employee(
+  'Viktor',
+  24,
+  'male',
+  'viktoryanenko11@gmail.com',
+  430,
+  'sales manager'
+);
+console.log(viktor.getEmployeeDetails());
